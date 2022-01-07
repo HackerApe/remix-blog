fdba7604894f9c1d4638088a84e9dc2eeedd4444
Dmitriy Shin <dmitriyshin.io@Dmitriys-MacBook-Pro.local>
2022-01-07 13:34:17 -0500

    added title and body validation when adding a new post

--- app/routes/posts/new.tsx
+++ app/routes/posts/new.tsx
@@ -1,5 +1,14 @@
-import { Link, redirect } from "remix"
+import { Link, redirect, useActionData, json, ActionFunction } from "remix"
 import { db } from "~/utils/db.server"
+
+const validateTitle = (title: string) => {
+  if (typeof title !== "string" || title.length < 3)
+    return "Title should be at least 3 characters long"
+}
+const validateBody = (body: string) => {
+  if (typeof body !== "string" || body.length < 10)
+    return "Body should be at least 10 characters long"
+}
 
 export const action = async ({ request }) => {
   const form = await request.formData()
@@ -8,12 +17,22 @@ export const action = async ({ request }) => {
   const body = form.get("body")
 
   const fields = { title, body }
+  const fieldErrors = { title: validateTitle(title), body: validateBody(body) }
+
+  if (Object.values(fieldErrors).some(Boolean)) {
+    console.log(fieldErrors)
+    return json({ fieldErrors, fields }, { status: 400 })
+  }
+
+  // Submitting to db
   const post = await db.post.create({ data: fields })
 
   return redirect(`/posts/${post.id}`)
 }
 
 export default function NewPost() {
+  const actionData = useActionData()
+
   return (
     <>
       <div className='page-header'>
@@ -32,11 +51,26 @@ export default function NewPost() {
               type='text'
               name='title'
               placeholder='Post Title'
+              defaultValue={actionData?.fields?.title}
             />
+            <div className='error'>
+              <p>
+                {actionData?.fieldErrors?.title && actionData.fieldErrors.title}
+              </p>
+            </div>
           </div>
           <div className='form-control'>
             <label htmlFor='title'>Title</label>
-            <textarea id='body' name='body'></textarea>
+            <textarea
+              id='body'
+              name='body'
+              defaultValue={actionData?.fields?.body}
+            ></textarea>
+            <div className='error'>
+              <p>
+                {actionData?.fieldErrors?.body && actionData.fieldErrors.body}
+              </p>
+            </div>
           </div>
           <button className='btn btn-block' type='submit'>
             Add Post
